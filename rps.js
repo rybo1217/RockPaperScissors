1cb8ac4c0b1524a74913be39fa73deec377ef296
rybo1217 <brownryan1217@yahoo.com>
2022-10-14 11:52:12 -0400

    STARTED

---
+++ rps.js
@@ -0,0 +1,43 @@
+
+
+let comAns = ["Rock","Paper","Scissors"]
+
+//const pAns=prompt("Choose Rock, Paper, or Scissors.")
+
+if(comAns == "Rock" && pAns=="Rock")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+
+}
+elif(comAns == "Rock" && pAns=="Paper")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Rock" && pAns=="Scissors")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Paper" && pAns=="Rock")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Paper" && pAns=="Paper")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Paper" && pAns=="Scissors")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Scissors" && pAns=="Rock")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Scissors" && pAns=="Paper")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
+elif(comAns == "Scissors" && pAns=="Scissors")
+{
+    console.log(comAns[Math.floor(Math.random() * 3)])
+}
